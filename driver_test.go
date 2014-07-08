26cf8c381e44bffca0a6d1d2da59708e5a5b5c58
Carl Sverre <carl@carlsverre.com>
2014-07-07 19:38:02 -0700

    better tests and more benchmarks

--- driver_test.go
+++ driver_test.go
@@ -2,7 +2,6 @@ package mysqldb
 
 import (
 	"database/sql"
-	"fmt"
 
 	. "gopkg.in/check.v1"
 )
@@ -56,21 +55,64 @@ func (s *DriverSuite) mustQuery(c *C, query string, args ...interface{}) *sql.Ro
 }
 
 func (s *DriverSuite) TestBasic(c *C) {
-	rows := s.mustQuery(c, "SHOW DATABASES")
+	for i := 0; i < 10; i++ {
+		s.db.Exec("INSERT INTO x (foo) VALUES (%s)", "asdf")
+	}
+
+	rows := s.mustQuery(c, "SELECT * FROM x")
+	count := 0
 
 	for rows.Next() {
+		var id int
 		var name string
 
-		err := rows.Scan(&name)
+		err := rows.Scan(&id, &name)
 		c.Assert(err, IsNil)
 
-		fmt.Printf("db_name: %s\n", name)
+		count += 1
+		c.Assert(id, Equals, count)
+		c.Assert(name, Equals, "asdf")
 	}
+
+	c.Assert(count, Equals, 10)
 }
 
-func (s *DriverSuite) BenchmarkBasic(c *C) {
+func (s *DriverSuite) BenchmarkBasicInsertWithEscape(c *C) {
 	query := "INSERT INTO x (foo) VALUES (%s)"
 	for i := 0; i < c.N; i++ {
 		s.db.Exec(query, "asdf")
+	}
+}
+
+func (s *DriverSuite) BenchmarkBasicInsertNoEscape(c *C) {
+	query := "INSERT INTO x (foo) VALUES ('asdf')"
+	for i := 0; i < c.N; i++ {
+		s.db.Exec(query)
+	}
+}
+
+func (s *DriverSuite) BenchmarkBasicKVSelectWithEscape(c *C) {
+	var id int
+	var name string
+
+	s.db.Exec("INSERT INTO x (foo) VALUES ('asdf')")
+
+	for i := 0; i < c.N; i++ {
+		rows := s.db.QueryRow("SELECT * FROM x WHERE id = %s", 1)
+		err := rows.Scan(&id, &name)
+		c.Assert(err, IsNil)
+	}
+}
+
+func (s *DriverSuite) BenchmarkBasicKVSelectNoEscape(c *C) {
+	var id int
+	var name string
+
+	s.db.Exec("INSERT INTO x (foo) VALUES ('asdf')")
+
+	for i := 0; i < c.N; i++ {
+		rows := s.db.QueryRow("SELECT * FROM x WHERE id = 1")
+		err := rows.Scan(&id, &name)
+		c.Assert(err, IsNil)
 	}
 }
